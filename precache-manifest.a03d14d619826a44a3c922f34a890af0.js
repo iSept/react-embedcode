806be0ab21de6c6a81cc39a0ddaf08e2b331646b
Daniel Bollom <daniel@bluetonguedigital.com.au>
2020-10-22 23:53:21 +1100

    Updates

---
+++ precache-manifest.a03d14d619826a44a3c922f34a890af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d066d158b3302d4746dceaa3890408",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4c0afc2c6e2f6a2e417",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "129de77a89b3aacc5e37",
+    "url": "./static/js/2.acfd6a05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.acfd6a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4c0afc2c6e2f6a2e417",
+    "url": "./static/js/main.137eff4d.chunk.js"
+  },
+  {
+    "revision": "60e76f0135faee924851",
+    "url": "./static/js/runtime-main.6833ea69.js"
+  }
+]);
